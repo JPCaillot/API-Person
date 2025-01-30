359fa1b18203294212c224c44bdd88fd4dc88ccc
JPCaillot <joaopcaillot@gmail.com>
2025-01-30 20:06:48 -0300

    Fixed Cors issue for front-end endpoint

---
+++ src/main/java/com/project/backend/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.project.backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Value("cors.origin")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] origins = allowedOrigins.split(",");
+
+        registry.addMapping("/**")
+                .allowedOrigins(origins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
+}
--- src/main/java/com/project/backend/controllers/PessoaController.java
+++ src/main/java/com/project/backend/controllers/PessoaController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/backend/pessoa")
+@CrossOrigin(origins = "http://localhost:4200")
 @Validated
 public class PessoaController {
     private final IPessoaService pessoaService;
